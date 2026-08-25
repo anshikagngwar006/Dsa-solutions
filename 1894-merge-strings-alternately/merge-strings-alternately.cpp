2b7893ea9e58c529c6fb5bacfdba086f6bed5910
ANSHIKA GANGWAR <anshikagngwar006@gmail.com>
2026-08-25 17:55:12 +0530

    Time: 4 ms (32.13%) | Memory: 8.7 MB (10.09%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string ans;
+        int i=0;
+        int j=0;
+        while(i<word1.length()&&j<word2.length()){
+            ans.push_back(word1[i]);
+            ans.push_back(word2[j]);
+            i++;j++;
+        }
+        if(word1.length()>word2.length()){
+           string t= word1.substr(i);
+        ans=ans+t;
+        return ans;
+    }
+    else{
+        string p= word2.substr(j);
+        ans+=p;
+        return ans;
+    }
+    }
+};
