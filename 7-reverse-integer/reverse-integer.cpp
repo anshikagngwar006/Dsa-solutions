c318671d88d89da5f9adf73d027152a9aef72ce8
ANSHIKA GANGWAR <anshikagngwar006@gmail.com>
2026-08-26 15:01:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (54.48%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int rev=0;
+        int nums;
+        
+        while(x!=0){
+            nums=x%10;
+            if(rev > INT_MAX / 10 ||
+               (rev == INT_MAX / 10 && nums > INT_MAX % 10)) {
+                return 0;
+            }
+
+            if(rev < INT_MIN / 10 ||
+               (rev == INT_MIN / 10 && nums < INT_MIN % 10)) {
+                return 0;
+            }
+            rev=(rev*10)+nums;
+            x=x/10;
+        }
+        return rev;
+    }
+};
