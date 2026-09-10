2967a01e1bc89de69116c5e7bd1ad60fa7b4b7e7
ANSHIKA GANGWAR <anshikagngwar006@gmail.com>
2026-09-10 19:41:19 +0530

    Time: 1 ms (43.04%) | Memory: 10.2 MB (22.53%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string ans="";
+        for(int i=0;i<s.length();i++){
+    if(isalnum(s[i])){
+        if(isupper(s[i])){
+            s[i]=tolower(s[i]);
+            ans.push_back(s[i]);
+        }
+        else {
+            ans.push_back(s[i]);
+        }
+    }
+        }
+    int i=0;
+    int j=ans.length()-1;
+    bool found=true;
+    while(i<=j){
+        if(ans[i]!=ans[j]){
+            found=false;
+        }
+        i++;
+        j--;
+    }
+
+return found;
+    }
+};
