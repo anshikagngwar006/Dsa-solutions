0defdac7a02a8b3bf0222bf68c64a1fba18f3685
ANSHIKA GANGWAR <anshikagngwar006@gmail.com>
2026-09-10 12:14:42 +0530

    Time: 4 ms (5.77%) | Memory: 18.9 MB (16.93%) - LeetSync

---
+++ 443-string-compression/string-compression.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        string ans;
+        int index=0;
+        int l=0;
+        int r=0;
+        int count=0;
+        while(r<chars.size()&&l<chars.size()){
+            if(chars[l]==chars[r]){
+                count++;
+                r++;
+            }
+            else if(chars[l]!=chars[r]){
+                ans.push_back(chars[l]);
+                if(count>1){
+                    ans=ans+to_string(count);
+                }
+                count=0;
+                l=r;
+
+            }
+            
+                
+            }
+                ans.push_back(chars[l]);
+                if(count>1){
+                    ans=ans+to_string(count);
+                }
+        for(int i=1;i< ans.length();i++){
+        chars[i]=ans[i];
+        index=i;
+        }
+        return index+1;
+
+    }
+};
