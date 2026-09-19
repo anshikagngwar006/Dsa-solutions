41eccea9099a9a38b7a37cefe1b350dcf1b0f988
ANSHIKA GANGWAR <anshikagngwar006@gmail.com>
2026-09-19 16:33:19 +0530

    Time: 2 ms (6.93%) | Memory: 9.3 MB (5.32%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -3,18 +3,38 @@ public:
     bool isSubsequence(string s, string t) {
         int m=s.length();
         int n=t.length();
-        int i=0;
-        int j=0;
-        while(i<m&&j<n){
-            if(s[i]==t[j]){
-                i++;
-                j++;
-            }
-            else{
-                j++;
-            }
+        int prev=-1;
+        int count=0;
+        unordered_map<char,vector<int>>mp;
+        for(int i=0;i<n;i++){
+        mp[t[i]].push_back(i);
         }
-        if(i==m){
+        for(int i=0;i<m;i++){
+            if(mp.find(s[i])==mp.end()){
+                return false;
+            }
+            else if(mp.find(s[i])!=mp.end()){
+               vector<int>indices=mp[s[i]];
+               for(int j=0;j<indices.size();j++){
+                if(indices[j]>prev){
+                    count++;
+                    prev=indices[j];
+                    break;
+                }/* isko directly upper bound function se bhi kar shkte the jaisevector<int> indices = mp[s[i]];
+
+auto it = upper_bound(indices.begin(), indices.end(), prev);
+
+if(it == indices.end()) {
+    return false;
+}
+
+prev = *it;
+count++;*/
+            }
+
+        }
+        }
+        if (count==m){
             return true;
         }
         return false;
