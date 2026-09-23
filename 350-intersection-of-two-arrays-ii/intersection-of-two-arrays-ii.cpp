9fa56e541a8784223b23bf1a7ffa646fd304a480
ANSHIKA GANGWAR <anshikagngwar006@gmail.com>
2026-09-23 19:51:02 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (57.55%) - LeetSync

---
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums1.size();i++){
+            mp[nums1[i]]++;
+        }
+        for(int j=0;j<nums2.size();j++){
+            if(mp.find(nums2[j])!=mp.end()){
+                if(mp[nums2[j]]>0){
+                    ans.push_back(nums2[j]);
+                    mp[nums2[j]]--;
+                }
+            }
+        }
+        return ans;
+    }
+};
